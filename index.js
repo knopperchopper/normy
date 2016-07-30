5966ab728ba3a12e9713ae2b2317ca5e286b43dc
knopperchopper <ryan@ryanknopp.com>
2016-07-30 08:38:57 -0400

    setting up module options validation and handler

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
+var Validator = require('jsonschema').Validator;
+var v = new Validator();
+
+
+// Define Options schema for validation
+var optionsSchema = {
+    "id": "/Options",
+    "type": "object:",
+    "properties": {
+        "forceProtocol": {
+            "enum": ["http", "https", "none"]
+        },
+        "forceWww": {
+            "enum": ["www", "no-www", "none"]
+        },
+        "forceTrailingSlash": {
+            "enum": ["trim", "keep", "none"]
+        },
+        "forceCase": {
+            "enum": ["lower", "upper", "none"]
+        },
+        "forceCaseQuery": {
+            "enum": ["lower", "upper", "none"]
+        },
+        "redirectType": {
+            "enum": ["301", "302"]
+        },
+    }
+}
+
+// Add schema to validator
+v.addSchema(optionsSchema, '/Options');
+
+
+
 var defaults = {
-    forceProtocol: 'none',              // Options: http, https, none
+    forceProtocol: "none",              // Options: http, https, none
     forceWww: 'www',                    // Options: www, no-www, none
     forceTrailingSlash: 'trim',         // Options: trim, keep, none
     forceCase: 'lower',                 // Options: lower, upper, none
     forceCaseQuery: 'none',             // Options: lower, upper, none
-    redirectType: 301                   // Options: 301, 302
+    redirectType: "301"                 // Options: 301, 302
 }
 
 
 module.exports = function(opts){
     
+    // Handle Options: If option is not passed, use default
+    opts.forceProtocol = opts.forceProtocol || defaults.forceProtocol;
+    opts.forceWww = opts.forceWww || defaults.forceWww;
+    opts.forceTrailingSlash = opts.forceTrailingSlash || defaults.forceTrailingSlash;
+    opts.forceCase = opts.forceCase || defaults.forceCase;
+    opts.forceProtocol = opts.forceCaseQuery || defaults.forceCaseQuery;
+    opts.forceProtocol = opts.redirectType || defaults.redirectType;
     
     
     return function(req, res, next){
@@ -22,7 +64,7 @@ module.exports = function(opts){
         
         // Force Lowercase Path 
         if(opts.forceCase !== undefined){
-            if()
+            
             if (/[A-Z]/.test(urlPath)) { 
                 redirectRequired = true; 
                 urlPath = urlPath.toLowerCase(); 
@@ -42,3 +84,18 @@ module.exports = function(opts){
 
     }
 }
+
+function HandleOptions(opts){
+    opts.forceProtocol = opts.forceProtocol || defaults.forceProtocol;
+    opts.forceWww = opts.forceWww || defaults.forceWww;
+    opts.forceTrailingSlash = opts.forceTrailingSlash || defaults.forceTrailingSlash;
+    opts.forceCase = opts.forceCase || defaults.forceCase;
+    opts.forceCaseQuery = opts.forceCaseQuery || defaults.forceCaseQuery;
+    opts.redirectType = opts.redirectType || defaults.redirectType;
+    
+    v.validate(opts, optionsSchema, {throwError: true});
+    
+    return opts;
+}
+
+console.log(HandleOptions({forceWww: "no-www"}));
